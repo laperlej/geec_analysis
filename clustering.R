8909e2b4be59a1d89567c4746485498a2cf215ff
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-05-02 14:29:57 -0400

    improved clustering and added heatmap

--- clustering.R
+++ clustering.R
@@ -8,12 +8,14 @@ get_dendro <- function(matrix){
   return(hclust(as.dist(1-matrix)))
 }
 
-analyse_clusters <- function(clust, labels, matrix, k){
+analyse_clusters <- function(clust, labels, matrix, dendro){
   clusters_info <- NULL
-  for (i in 1:k) {
+  count<-1
+  for (i in unique(clust[dendro$order])) {
     cluster <- row.names(matrix[clust==i,])
     cluster_info <- analyse_cluster(cluster, labels)
-    cluster_name <- rep(paste("c", i, sep=""), 6)
+    cluster_name <- rep(paste("c", count, sep=""), 6)
+    count <- count + 1
     cluster_len <- rep(length(cluster), 6)
     cluster_info <- cbind(cluster_name, cluster_len, cluster_info)
     clusters_info <- rbind(clusters_info, cluster_info)
@@ -22,6 +24,8 @@ analyse_clusters <- function(clust, labels, matrix, k){
 }
 
 analyse_cluster <- function(cluster, labels){
+  write(cluster, "/Users/Jon/Desktop/cluster")
+  write(as.vector(labels$datasets), "/Users/Jon/Desktop/labels")
   clust_labels <- labels[labels$datasets %in% cluster,]
   top5_assay <- sort(table(clust_labels$assay), decreasing=T)[1:5]
   top5_celltype <- sort(table(clust_labels$cellType), decreasing=T)[1:5]
@@ -106,25 +110,45 @@ analyse_cluster <- function(cluster, labels){
   return(cluster_info)
 }
 
+make_heatmap <- function(matrix, outputRaster){
+  m.raster <- raster(matrix)
+  
+  #make the color function
+  n=256
+  cols <- colorRampPalette(rev(brewer.pal(11,"RdBu")))(n)
+  breaks <- c(seq(-1, 0, length.out=n/2)-0.001, 0, seq(0, 1, length.out=n/2)+0.001)
+  
+  #create the image
+  png(outputRaster, width=2000, height=2000)
+  plot(m.raster, col=cols, breaks=breaks, interpolate=FALSE, xlim=c(0,1), ylim=c(0,1), legend=FALSE, axes=FALSE, bty="n", box=FALSE)
+  plot(m.raster, legend.only=TRUE, col=cols, breaks=breaks, axis.args=list(at=seq(-1, 1, 1), labels=seq(-1, 1, 1)))
+  dev.off()
+}
+
 main <- function(){
   args <- commandArgs(trailingOnly=T)
-  matrixFile <- "/Users/Jon/Desktop/ARI/hg19.mat"
-  labelFile <- "/Users/Jon/Desktop/ARI/all.csv"
-  nb_clusters <- 6
+  matrixFile <- "/Users/Jon/Desktop/collabo/hg19.mat"
+  labelFile <- "/Users/Jon/Desktop/collabo/collab.csv"
+  k <- 12
   
   #read files
   matrix <- as.matrix(read.csv(matrixFile, header=T, sep='\t', row.names=1, as.is=T, check.names=F))
-  labels <- read.csv(labelFile)
+  labels <- read.csv(labelFile, header=T)
   
   #reorder alphabetically
+  labels <- labels[order(labels$datasets),]
+  row.names(labels) <- NULL
+  matrix <- matrix[as.vector(labels$datasets), as.vector(labels$datasets)]
+  
+  #submatrix
   matrix <- matrix[order(row.names(matrix)), order(row.names(matrix))]
   
   #cluster
   dendro <- get_dendro(matrix)
-  
-  clust <- cutree(dendro, k=nb_clusters)
-  clusters_info <- analyse_clusters(clust, labels, matrix, nb_clusters)
-  write.table(clusters_info, file="/Users/Jon/Desktop/test.txt", sep="\t", row.names=F, col.names=F)
+  clust <- cutree(dendro, k=k)
+  clusters_info <- analyse_clusters(clust, labels, matrix, dendro)
+  write.table(clusters_info, file="/Users/Jon/Desktop/collabo/test.txt", sep="\t", row.names=F, col.names=F)
+  make_heatmap(matrix[dendro$order, dendro$order], "/Users/Jon/Desktop/collabo/test.png")
 }
 
 main()
